3e53c894beb34ee0991df375ea599711eee60d34
Banu Darius Matei <30901594+TheNumber5@users.noreply.github.com>
2022-01-08 18:14:48 +0200

    Add files via upload

--- RefractionSurface.cs
+++ RefractionSurface.cs
@@ -7,12 +7,13 @@ public class RefractionSurface : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other) {
 		ior = HuygensPrinciple.instance.ior;
 		Particle particle = other.gameObject.GetComponent<Particle>();
+		particle.hasHitRefractiveSurface=true;
 		float incident = Mathf.Deg2Rad*Vector3.Angle(-particle.velocity, this.transform.up);
-		float refracted = Mathf.Rad2Deg*Mathf.Asin(Mathf.Sin(incident)/ior);
+		float refracted = Mathf.Asin(Mathf.Sin(incident)/ior);
 		float newSpeed = particle.velocity.magnitude/ior;
 		if(particle.velocity.x>=0)
-		particle.velocity = (Quaternion.AngleAxis(refracted, new Vector3(0f, 0f, 1f))*(-this.transform.up)).normalized*newSpeed;
+		particle.velocity = new Vector3(Mathf.Sin(refracted)*newSpeed, -Mathf.Cos(refracted)*newSpeed);
 		else
-		particle.velocity = (Quaternion.AngleAxis(-refracted, new Vector3(0f, 0f, 1f))*(-this.transform.up)).normalized*newSpeed;
+		particle.velocity = new Vector3(-Mathf.Sin(refracted)*newSpeed, -Mathf.Cos(refracted)*newSpeed);
 	}
 }
